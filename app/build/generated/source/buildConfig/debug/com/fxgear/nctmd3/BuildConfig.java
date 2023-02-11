975e312d9c326ae8701970e77d14c2e4f3784d2b
lswcharming <templateclass@gmail.com>
2023-02-12 02:02:05 +0900

    Base Release

---
+++ app/build/generated/source/buildConfig/debug/com/fxgear/nctmd3/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fxgear.nctmd3;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fxgear.nctmd3";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
